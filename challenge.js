c5ffdf84364ea185f091e8b60fd7e6a8658a2223
Kersjes, Alex G <alexkersjes@live.nl>
2020-04-24 23:10:28 +0200

    added a challenge clock

--- challenge.js
+++ challenge.js
@@ -37,11 +37,25 @@ class Challenge
 		{
 			this.finish(client);
 		}, this.Endingtime - Date.now());
+		this.StatusUpdater = setInterval(() =>
+		{
+			let delta = Math.abs(this.Endingtime - Date.now() / 1000);
+			const days = Math.floor(delta / 86400);
+			delta -= days * 86400;
+			const hours = Math.floor(delta / 3600) % 24;
+			delta -= hours * 3600;
+			const minutes = Math.floor(delta / 60) % 60;
+			delta -= minutes * 60;
+			const seconds = delta % 60;
+			client.user.setStatus(`Time Remaining: ${days ? days + ':' : ''}${hours}:${minutes}:${seconds}`);
+		}, 1000 * 60);
 	}
 	finish(client)
 	{
 		// subtotal all collected scores
 		// add scores to totals
+		clearInterval(this.StatusUpdater);
+		client.setStatus('Challenge over!~~');
 		if(client.CurrentChallenge != this)
 		{
 			return;
